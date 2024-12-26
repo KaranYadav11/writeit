1d0a97560455ad5d090e4cc121c64eb6b93c0ae5
KaranYadav11 <11yadavkaran@gmail.com>
2024-12-26 00:57:43 -0600

    first commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,23 +6,35 @@ import postRoutes from "./routes/postRoute.js";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import userRoutes from "./routes/userRoute.js";
-// import path from "path";
+import path from "path";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use("/api/v1/posts", postRoutes);
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/auth", authRoutes);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
-// app.use(express.static(path.join(__dirname, "/Frontend/dist")));
-// app.get("*", (_, res) => {
-//   res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
-// });
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.path}`);
+  next();
+});
+
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
